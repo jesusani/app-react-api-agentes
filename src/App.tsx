8ecb31472f5d1d5411fbcdbf3a2887201eeed064
jesusani <jesus.ani@gmail.com>
2025-11-17 12:25:33 +0100

    reparando ortografia

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import {
   createBrowserRouter,
   redirect,
 } from "react-router-dom";
-import agentes from "./components/agentes.jsx";
-import agentbusqueda from "./components/agentesbusqueda.jsx";
+import Agentes from "./Components/agentes.jsx";
+import Agentbusqueda from "./Components/agentesbusqueda.jsx";
 import react from "react";
-import layout from "./components/layout.tsx";
-import home from "./components/home.jsx";
-import formagentes from "./components/formagentes.jsx";
-import formtecnicas from "./components/formtecnicas.jsx";
-import tecnicas from "./components/tecnicas.jsx";
-import tecnicabusqueda from "./components/tecnicasbusqueda.jsx";
-import { protectedloader } from "./components/ProtectedLoader.tsx";
+import Layout from "./Components/layout.tsx";
+import Home from "./Components/home.jsx";
+import Formagentes from "./Components/formagentes.jsx";
+import Formtecnicas from "./Components/formtecnicas.jsx";
+import Tecnicas from "./Components/tecnicas.jsx";
+import Tecnicabusqueda from "./Components/tecnicasbusqueda.jsx";
+import { protectedloader } from "./Components/ProtectedLoader.tsx";
 
 
 
@@ -20,52 +20,52 @@ const router = createBrowserRouter([
   {
     id: "root",
     path: "/",
-    component: layout,
+    Component: Layout,
     children: [
       {
         index: true,
-        component: home,
+        Component: Home,
       },
       {
         path: "protected/agentes",
         loader: protectedloader,
-        component: agentes,
+        Component: Agentes,
       }, 
       {
         path: "protected/agentesbusqueda",
         loader: protectedloader,
-        component: agentbusqueda,
+        Component: Agentbusqueda,
       },
       {
         path: "protected/agentes/create", 
         loader: protectedloader, // ruta para crear un nuevo registro
-        component: () => <formagentes apiurl="https://api-nodejs-agentes.onrender.com/api/v1/agentes" />,
+        Component: () => <Formagentes apiUrl="https://api-nodejs-agentes.onrender.com/api/v1/agentes" />,
       },
 
       {      
         path: "protected/tecnicas",
         loader: protectedloader,
-        component: () => <tecnicas apiurl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
+        Component: () => <Tecnicas apiUrl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
        }, 
        {
          path: "protected/tecnicasbusqueda",
          loader: protectedloader,
-         component: tecnicabusqueda,
+         Component: Tecnicabusqueda,
        },
        {
          path: "protected/tecnicas/create",  // ruta para crear un nuevo registro
           loader: protectedloader,
-         component: () => <formtecnicas apiurl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
+         Component: () => <Formtecnicas apiUrl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
        },
        {
         path: "protected/agentes/edit/:id",  // ruta para editar un registro existente
         loader: protectedloader,
-        component: () => <formagentes apiurl="https://api-nodejs-agentes.onrender.com/api/v1/agentes" />,
+        Component: () => <Formagentes apiUrl="https://api-nodejs-agentes.onrender.com/api/v1/agentes" />,
       },
       {
        path: "protected/tecnicas/edit/:id",  // ruta para editar un registro existente
         loader: protectedloader,
-       component: () => <formtecnicas apiurl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
+       Component: () => <Formtecnicas apiUrl="https://api-nodejs-agentes.onrender.com/api/v1/tecnicas" />,
      },
     ],
   },
@@ -80,6 +80,6 @@ const router = createBrowserRouter([
 
 export default function app() {
   return (
-    <routerprovider router={router} fallbackelement={<p>initial load...</p>} />
+    <RouterProvider router={router} fallbackElement={<p>initial load...</p>} />
   );
 }
